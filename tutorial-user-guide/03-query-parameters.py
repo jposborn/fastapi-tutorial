d1ff4e7459eb0103a79423d5cc28c9d7ec31d283
John Osborn <john.osborn@gigaclear.com>
2025-11-16 14:49:07 +0000

    Add 3-query parameters

---
+++ tutorial-user-guide/03-query-parameters.py
@@ -0,0 +1,55 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+# Query parameters with default values
+@app.get("/items_1/")
+async def read_item_1(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+
+# Query parameter with optional values
+@app.get("/items_2/{item_id}")
+async def read_item_2(item_id: str, q: str | None = None):
+    if q:
+        return {"item_id": item_id, "q": q}
+    return {"item_id": item_id}
+
+
+# Query parameter with str and bool with type conversion. All these (and more) are valid 
+# /items/foo?short=1, /items/foo?short=true, /items/foo?short=True, /items/foo?short=on
+@app.get("/items_3/{item_id}")
+async def read_items_3(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+
+# Multiple path and query parameters declared at the same time
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item_1(
+    user_id: int, item_id: str, q: str | None = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+
+# Required query parameter (item_id and needy have no defaults)
+@app.get("/items_4/{item_id}")
+async def read_user_item_2(item_id: str, needy: str):
+    item = {"item_id": item_id, "needy": needy}
+    return item
+
